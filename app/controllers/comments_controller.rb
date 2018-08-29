0091767640aa91f3841fb6fdd20abcbcb4e2ae97
sristikashakya <sristikashakya123@gmail.com>
2018-08-29 18:50:47 +0545

    Post Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(comment_params.merge(restaurant_id: current_restaurant.id))
+		if @comment.valid?
+			redirect_to root_path
+		else
+			flash[:alert] = "Invalid attributes."
+			redirect_to root_path
+		end
+	end
+
+	private 
+
+	def comment_params
+		params.require(:comment).permit(:text, :post_id)
+	end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
 	belongs_to :post
 	belongs_to :restaurant
+	validates :text, :restaurant_id, :post_id, presence: true
 end
