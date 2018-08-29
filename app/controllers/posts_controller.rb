4d8f0bef4106794948ad4f0f1150f5cd0fdb8ba6
sristikashakya <sristikashakya123@gmail.com>
2018-08-29 16:59:13 +0545

    Update post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,18 @@ class PostsController < ApplicationController
 	def index
   		@posts = Post.all.order('created_at DESC')
 	end
+	def edit
+		@post = Post.find(params[:id])
+	end
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		if @post.valid?
+			redirect_to root_path
+		else
+			render :edit, status: :unprocessable_entity
+		end
+	end
 
 	private
 
