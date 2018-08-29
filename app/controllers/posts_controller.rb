8e420569cb22256fbe80ad6e7c07466a7ad69fcc
sristikashakya <sristikashakya123@gmail.com>
2018-08-29 17:18:51 +0545

    use filters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	before_action :authenticate_restaurant!, only: [:new, :create]
+	before_action :is_owner?, only: [:edit, :update]		
 	def create
 		@post = current_restaurant.posts.create(post_params)
 			if @post.valid?
@@ -28,7 +29,9 @@ class PostsController < ApplicationController
 	end
 
 	private
-
+	def is_owner?
+	   redirect_to root_path if Post.find(params[:id]).user != current_user
+	end
 	def post_params
 		params.require(:post).permit(:restaurant_id, :photo, :description)
 	end
