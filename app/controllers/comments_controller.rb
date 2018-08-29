9291c421239333d3cc296358e9d45e446bbb6ba8
sristikashakya <sristikashakya123@gmail.com>
2018-08-29 20:46:07 +0545

    Delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+	before_action :is_owner?, only: :destroy
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(comment_params.merge(restaurant_id: current_restaurant.id))
@@ -9,8 +10,18 @@ class CommentsController < ApplicationController
 			redirect_to root_path
 		end
 	end
+	def destroy
+		 @comment = Comment.find(params[:id])
+		@comment.destroy
+		redirect_to root_path
+	end
 	private 
-
+	def is_owner?
+		@comment = Comment.find(params[:id])
+		if @comment.restaurant != current_restaurant
+			redirect_to root_path
+		end
+	end
 	def comment_params
 		params.require(:comment).permit(:text, :post_id)
 	end
